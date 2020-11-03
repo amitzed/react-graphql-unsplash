0c9714509f7ec50f12fd0a8bfb1876b974f08887
amitzed <amitzmn@gmail.com>
2020-11-03 15:18:53 -0500

    Created Resolvers for GraphQL

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { ApolloServer, gql } = require('apollo-server');
 const axios = require('axios');
 
+const KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
+
 const typeDefs = gql`
   type URL {
     regular: String
@@ -22,3 +24,22 @@ const typeDefs = gql`
     photos: [Photo]
   }
 `;
+
+const resolvers = {
+  Query: {
+    photos: async () => {
+      try {
+        const photos = await axios.get(`https://api.unsplash.com/photos/?client_id=${KEY}`)
+
+        return photos.data.map(({ id, title, urls, user }) => ({
+          id,
+          title,
+          urls,
+          user
+        }))
+      } catch (error) {
+        throw error
+      }
+    }
+  }
+}
