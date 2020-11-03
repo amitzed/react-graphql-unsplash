3f06c03bbc9d0efaf866ab73bb53924f6eaebc93
amitzed <amitzmn@gmail.com>
2020-11-03 15:39:47 -0500

    Created Apollo server

--- app.js
+++ app.js
@@ -43,3 +43,11 @@ const resolvers = {
     }
   }
 }
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen()
+.then(({ url }) => console.log(`Server is ready at ${url}`))
